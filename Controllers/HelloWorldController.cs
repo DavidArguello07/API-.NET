1e8b5f5f13a4f6ed710554d1a3c1d21fb198a5d8
DavidArguello07 <jesussagastume2000@gmail.com>
2023-11-24 14:44:00 -0600

    Api funcional

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,23 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace webapi.Controllers;
 
 [ApiController]
-[Route("api2/[controller]")]
+[Route("api/[controller]")]
 public class HelloWorldController : ControllerBase
 {
     IHelloworldService helloworldServices;
+
+    TareasContext dbcontext;
     private readonly ILogger<HelloWorldController> _logger;
     //Recibir la dependencia de IHelloworldService a través del constructor
-    public HelloWorldController(IHelloworldService helloworld, ILogger<HelloWorldController> logger)
+    public HelloWorldController(TareasContext db, IHelloworldService helloworld, ILogger<HelloWorldController> logger)
     {
         _logger = logger;
         // this.helloworldServices = helloworld;
         helloworldServices = helloworld;
+        dbcontext = db;
     }
     [HttpGet]
     public IActionResult Get()
     {
         _logger.LogDebug("Logging from Get(). Hi!");
         return Ok(helloworldServices.GetHelloWorld());
+    }
+
+    [HttpGet]
+    [Route("createdb")]
+    public IActionResult CreateDatabase()
+    {//validar si la base de datos existe
+        dbcontext.Database.EnsureCreated();
+
+        return Ok("Database created");
     }
 
     // [Route("get/bye")]
